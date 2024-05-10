4385cbd2b92f214b231b918ef423b650ea962aa5
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-05-10 20:43:39 +0300

    feat(json-parser,step2): parse key value pairs ✨

--- json-parser/src/lexer.rs
+++ json-parser/src/lexer.rs
@@ -6,6 +6,21 @@ use anyhow::Result;
 pub enum Token {
     LBraces,
     RBraces,
+    Literal(String),
+    Colon,
+    Coma,
+}
+
+impl Token {
+    fn literal(&self) -> String {
+        match self {
+            Token::LBraces => String::from("{"),
+            Token::RBraces => String::from("}"),
+            Token::Literal(s) => s.clone(),
+            Token::Colon => String::from(":"),
+            Token::Coma => String::from(","),
+        }
+    }
 }
 
 pub struct Lexer<'a> {
@@ -24,23 +39,20 @@ impl<'a> Lexer<'a> {
     pub fn lex(&mut self) -> Result<Vec<Token>> {
         let mut tokens = Vec::<Token>::new();
 
-        // File is empty
+        // File is empty.
         if self.peek().is_none() {
             anyhow::bail!("Empty JSON file is invalid, expected '{{}}'")
         }
 
         while let Some(char) = self.read() {
-            if is_newline(char) {
-                self.eat_newline();
-                continue;
-            }
-            if is_whitespace(char) {
-                self.eat_whitespace();
-                continue;
-            }
             match char {
                 '{' => tokens.push(Token::LBraces),
-                '}' => tokens.push(Token::RBraces),
+                '}' => {
+                    anyhow::ensure!(tokens[tokens.len() - 1] != Token::Coma, "Trailing Coma");
+                    tokens.push(Token::RBraces);
+                }
+                '"' => tokens.append(&mut self.parse_kv()?),
+                ',' => tokens.push(Token::Coma),
                 c => {
                     anyhow::bail!("Unexpected token: {} at line {}", c, self.line)
                 }
@@ -60,9 +72,22 @@ impl<'a> Lexer<'a> {
         Ok(tokens)
     }
 
-    /// Get the current char.
+    /// Strip newline and whitespace and return the next valid char.
     fn read(&mut self) -> Option<char> {
-        self.input.next()
+        let mut ch: Option<char> = None;
+
+        for char in self.input.by_ref() {
+            if is_newline(char) {
+                self.line += 1;
+                continue;
+            } else if is_whitespace(char) {
+                continue;
+            } else {
+                ch = Some(char);
+                break;
+            }
+        }
+        ch
     }
 
     /// Peak ahead into the input.
@@ -70,15 +95,67 @@ impl<'a> Lexer<'a> {
         self.input.peek().copied()
     }
 
-    /// If we jumped into a new line, increase line number and consume char without tokenization.
-    fn eat_newline(&mut self) {
-        self.line += 1;
-        self.input.next();
+    /// Read string between double quotes.
+    ///
+    /// ## Errors
+    /// - If string is not terminated.
+    /// - If `:` is encountered before closing quote.
+    fn read_string(&mut self) -> Result<Token> {
+        let mut s = String::new();
+        let mut ended = false;
+
+        for c in self.input.by_ref() {
+            match c {
+                '"' => {
+                    ended = true;
+                    break;
+                }
+                ':' => anyhow::bail!("Expected closing quote "),
+                ch => s.push(ch),
+            }
+        }
+
+        if !ended {
+            anyhow::bail!("Unterminated string at line: {}", self.line);
+        }
+        Ok(Token::Literal(s))
     }
 
-    /// Consume the whitespace without tokenization.
-    fn eat_whitespace(&mut self) {
-        self.input.next();
+    /// Prase "key":"value".
+    fn parse_kv(&mut self) -> Result<Vec<Token>> {
+        let mut tokens = Vec::<Token>::new();
+
+        // Parse key.
+        let key = self.read_string()?;
+        tokens.push(key);
+
+        // Parse Colon.
+        if self
+            .read()
+            .is_some_and(|c| c.to_string() == Token::Colon.literal())
+        {
+            tokens.push(Token::Colon);
+        } else {
+            anyhow::bail!("Invalid expression at line {}, Expected ':'.", self.line)
+        };
+
+        // Parse Value.
+        if let Some(char) = self.read() {
+            match char {
+                '"' => {
+                    let literal = self.read_string()?;
+                    tokens.push(literal);
+                }
+                // parse other data types
+                _ => todo!(),
+            };
+        } else {
+            anyhow::bail!(
+                "Invalid expression at line {}, Expected 'value'.",
+                self.line
+            )
+        }
+        Ok(tokens)
     }
 }
 
@@ -87,7 +164,6 @@ fn is_newline(char: char) -> bool {
     char == '\n' || char == '\r'
 }
 
-/// Check if char is a whitespace.
 fn is_whitespace(char: char) -> bool {
     char == ' '
 }
