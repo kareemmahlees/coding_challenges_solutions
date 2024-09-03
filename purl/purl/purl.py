9c54b5d93861ba7f009b374575adab934ecc183d
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-09-03 13:21:51 +0000

    docs: add help text for command arguments 📝

--- purl/purl/purl.py
+++ purl/purl/purl.py
@@ -11,13 +11,31 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    url: str = typer.Argument(),
-    method: Annotated[RequestMethod, typer.Argument()] = RequestMethod.GET,
-    items: Annotated[Optional[List[str]], typer.Argument()] = None,
-    verbose: Annotated[bool, typer.Option()] = False,
-    offline: Annotated[bool, typer.Option()] = False,
-    form: Annotated[bool, typer.Option("--form", "-f")] = False,
+    url: str = typer.Argument(help="Server url to send request to."),
+    method: Annotated[
+        RequestMethod, typer.Argument(help="Request method.")
+    ] = RequestMethod.GET,
+    items: Annotated[
+        Optional[List[str]],
+        typer.Argument(
+            help="Headers and Json/Form data you which to send with the request.",
+        ),
+    ] = None,
+    verbose: Annotated[
+        bool,
+        typer.Option("--verbose", "-v", help="Show more info about sent requests."),
+    ] = False,
+    offline: Annotated[
+        bool, typer.Option(help="Only construct the request but don't send it.")
+    ] = False,
+    form: Annotated[
+        bool,
+        typer.Option("--form", "-f", help="Inidicate that sent data is form data."),
+    ] = False,
 ):
+    """
+    Basically `curl`, but more powerfull.
+    """
     parsed_url = UrlParser.parse(url)
 
     parsed_items = ItemsParser.parse(items)
